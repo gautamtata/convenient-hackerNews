0ca4186f33766ca8ce3b58e34eae371ea58ae968
Gautam Tata <gautamtata@gmail.com>
2019-07-22 17:43:44 -0700

    removing redundancy

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Dropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
